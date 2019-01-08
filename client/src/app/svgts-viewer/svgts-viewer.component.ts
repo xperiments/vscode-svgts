3598e2afebe022cc15d7db78fa9bef6ed08af7f3
Pedro Casaubon <xperiments@gmail.com>
2019-01-08 09:17:04 +0100

    Send messages to vscode

--- client/src/app/svgts-viewer/svgts-viewer.component.ts
+++ client/src/app/svgts-viewer/svgts-viewer.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
-import { IconsService } from './services/icons.service';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { IconsDataService } from './services/icons-data.service';
+import { IconsService } from './services/icons.service';
+import { VscodeService } from './services/vscode.service';
+import { SvgTsViewerIconsComponent } from './svgts-viewer-icons/svgts-viewer-icons.component';
+
+// https://code.visualstudio.com/api/extension-guides/webview
+
 @Component({
   selector: 'svgts-viewer',
   templateUrl: './svgts-viewer.component.html',
   styleUrls: ['./svgts-viewer.component.scss']
 })
-export class SvgTsViewerComponent {
-  constructor(public icons: IconsService, public iconsData: IconsDataService) {}
+export class SvgTsViewerComponent implements OnInit {
+  @ViewChild('viewerIcons')
+  public viewerIcons: SvgTsViewerIconsComponent;
+  constructor(public icons: IconsService, public iconsData: IconsDataService, private _vscode: VscodeService) {}
+
+  public ngOnInit() {
+    this._vscode.editor.postMessage({ command: 'loaded' });
+  }
+
+  @HostListener('window:message', ['$event'])
+  public onPostMessage(event: MessageEvent) {
+    const message: { command: string; data: any } = event.data;
+    if (!message) {
+      return;
+    }
+
+    switch (message.command) {
+      case 'svgts.command.setBrowsingMode': {
+        this.icons.setBrowsingMode(message.data as IconsService['browsingMode']);
+        break;
+      }
+      case 'svgts.command.iconDetail': {
+        this.viewerIcons.viewList
+          .filter(element => {
+            return element.iconFile.name === (message.data as string);
+          })
+          .map(element => {
+            element.iconClick();
+            element.showDetail();
+            element.detectChanges();
+          });
+
+        break;
+      }
+    }
+  }
 }
